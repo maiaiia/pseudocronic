f8325dce9e9e57fdbc87ffb180e7b15614532166
Violeta-16 <ungureanuvioletamihaela@gmail.com>
2025-11-08 22:59:55 +0200

    refactor run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run(
-        "backend.ai_powered_functionalities.main:app",
+        "backend.src.server:app",
         host="127.0.0.1",
         port=8000,
         reload=True
